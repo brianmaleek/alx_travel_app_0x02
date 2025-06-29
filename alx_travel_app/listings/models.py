786f23ac95038287ce8a78307230a778ab4e3c9c
Brian Maleek <maleekmalik@gmail.com>
2025-06-29 18:18:01 +0300

    Add Payment model with status and method choices for bookings

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -109,3 +109,41 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review {self.id} for {self.listing.title} by {self.reviewer.username}"
+
+class Payment(models.Model):
+    PAYMENT_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+        ('refunded', 'Refunded'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    PAYMENT_METHOD_CHOICES = [
+        ('chapa', 'Chapa'),
+
+    ]
+
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='payments')
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    transaction_id = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(
+        max_length=20,
+        choices=PAYMENT_STATUS_CHOICES,
+        default='pending'
+    )
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHOD_CHOICES,
+        default='chapa'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Payment {self.id} for Booking {self.booking.id} - {self.status}"
+
+
