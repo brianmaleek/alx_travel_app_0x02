f19edd894bcd57ff7207f6e82e4d295d5b214c3a
Brian Maleek <maleekmalik@gmail.com>
2025-06-29 18:17:42 +0300

    Import os module and add CHAPA_SECRET_KEY configuration to settings.py

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Application API Key
+CHAPA_SECRET_KEY = os.getenv('CHAPA_SECRET_KEY')
 
 # Application definition
 
